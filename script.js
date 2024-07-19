ee18fe9af8051d410c0310ace72b5999011dc516
lucianatux <lucianatux@yahoo.com.ar>
2024-07-19 11:55:10 -0300

    botones

--- script.js
+++ script.js
@@ -1,4 +1,168 @@
 document.addEventListener("DOMContentLoaded", function () {
+    const parkImg = document.getElementById("park-img");
+    const options = document.querySelectorAll('input[name="options"]');
+    const tutorialDiv = document.getElementById("tutorial-div");
+    const talkDiv = document.getElementById("talk-div");
+    const answerDiv = document.getElementById("answer-div");
+  
+    const tutorial = [
+      "<p>Esta experiencia interactiva te mostrará cómo nuestro mundo interno: nuestros gustos, sensaciones, recuerdos, prejuicios e ideas cambian nuestra interpretación del mundo y las respuestas que damos</p>",
+      "<p> &larr; Selecciona las distintas opciones<br><br>Observa como cambian tus diálogos mentales y tu percepción de la realidad</p>",
+      "<p>¿Notaste cómo tu percepción de la historia cambió según tu subjetividad?<br><br>Esta experiencia nos recuerda que debemos ser conscientes de cómo nuestras opiniones, creencias, temores, gustos, etc. pueden distorsionar nuestra interpretación del mundo que nos rodea.</p>",
+      "<p>mlñkmnñhgknmñlhkmñ</p>",
+    ];
+  
+    const talk = [
+      "<p>Hey! Por fin te encuentro!</p>",
+      "<p>Te estaba buscando...!</p>",
+    ];
+    const answer = [
+      "<p>Hey! Me alegra verte</p>",
+      "<p>¿Qué quieres?</p>",
+      "<p>Lo siento, dime rápido, me tengo que ir...</p>",
+      "<p>... aquí estoy</p>",
+      "<p> qué irritante...!</p>",
+      "<p>...quiero estar solo/a</p>",
+      "<p>Me cuesta creerte, te llamé y me rechazaste las llamadas</p>",
+      "<p>Para lo que necesites, amiga, aquí estoy...</p>",
+
+    ];
+  
+    let cancelPresentation = false;
+    let timers = [];
+
+    const restartButton = document.getElementById('restart');
+restartButton.addEventListener('click', function() {
+    location.reload();
+  });
+  
+    function showElement(element) {
+      element.style.display = "block";
+      setTimeout(() => {
+        element.classList.add("unfade");
+        element.classList.remove("fade");
+      }, 10);
+    }
+  
+    function hideElement(element) {
+      element.classList.add("fade");
+      element.classList.remove("unfade");
+      setTimeout(() => {
+        element.style.display = "none";
+      }, 1000);
+    }
+  
+    function updateDivContent(div, content) {
+      hideElement(div);
+      setTimeout(() => {
+        div.innerHTML = content;
+        showElement(div);
+      }, 1000);
+    }
+  
+    function setPresentationTimeout(callback, delay) {
+      if (!cancelPresentation) {
+        const timer = setTimeout(callback, delay);
+        timers.push(timer);
+      }
+    }
+  
+    async function presentation() {
+      cancelPresentation = false;
+      updateDivContent(tutorialDiv, tutorial[0]);
+      await new Promise(resolve => setPresentationTimeout(resolve, 12000));
+      hideElement(tutorialDiv);
+  
+      await new Promise(resolve => setPresentationTimeout(resolve, 1000));
+      updateDivContent(talkDiv, talk[0]);
+      await new Promise(resolve => setPresentationTimeout(resolve, 3000));
+      hideElement(talkDiv);
+  
+      await new Promise(resolve => setPresentationTimeout(resolve, 1000));
+      updateDivContent(talkDiv, talk[1]);
+      await new Promise(resolve => setPresentationTimeout(resolve, 3000));
+      hideElement(talkDiv);
+  
+      await new Promise(resolve => setPresentationTimeout(resolve, 1000));
+      updateDivContent(tutorialDiv, tutorial[1]);
+      await new Promise(resolve => setPresentationTimeout(resolve, 7000));
+      hideElement(tutorialDiv);
+    }
+  
+    presentation();
+  
+    options.forEach(option => {
+      option.addEventListener("change", () => {
+        const effect = option.value;
+        applyEffect(effect);
+      });
+    });
+  
+    function applyEffect(effect) {
+      cancelPresentation = true;
+      timers.forEach(timer => clearTimeout(timer));
+      timers = [];
+  
+      hideElement(tutorialDiv);
+      hideElement(talkDiv);
+      hideElement(answerDiv);
+  
+      switch (effect) {
+        case "agrada":
+          parkImg.style.filter = "saturate(110%)";
+          updateDivContent(answerDiv, answer[0]);
+          break;
+        case "desagrada":
+          parkImg.style.filter = "saturate(70%)";
+          updateDivContent(answerDiv, answer[1]);
+          break;
+        case "apurado":
+          parkImg.style.filter = "hue-rotate(300deg)";
+          updateDivContent(answerDiv, answer[2]);
+          break;
+        case "relajado":
+          parkImg.style.filter = "saturate(110%)";
+          updateDivContent(answerDiv, answer[3]);
+          break;
+        case "tenso":
+          parkImg.style.filter = "hue-rotate(180deg)";
+          updateDivContent(answerDiv, answer[4]);
+          break;
+        case "triste":
+          parkImg.style.filter = "saturate(10%)";
+          updateDivContent(answerDiv, answer[5]);
+          break;
+        case "llamando":
+          parkImg.style.filter = "url('./assets/esc1.jpg')";
+          updateDivContent(answerDiv, answer[6]);
+          break;
+        case "regalo":
+          parkImg.style.filter = "saturate(100%)";
+          updateDivContent(answerDiv, answer[7]);
+          break;
+        case "discutiendo":
+          parkImg.style.filter = "url('./assets/esc5.jpg')";
+          // Lógica para "discutiendo"
+          break;
+        default:
+          parkImg.style.filter = "saturate(100%)";
+          break;
+      }
+    }
+  
+    function cambiarImagenSegunPantalla() {
+      const windowWidth = window.innerWidth;
+      parkImg.src = windowWidth <= 568 ? "./assets/esc4.jpg" : "./assets/esc3.jpg";
+    }
+  
+    window.addEventListener("resize", cambiarImagenSegunPantalla);
+    cambiarImagenSegunPantalla();
+  });
+  
+
+
+
+/*document.addEventListener("DOMContentLoaded", function () {
   const parkImg = document.getElementById("park-img");
   const options = document.querySelectorAll('input[name="options"]');
   const tutorialDiv = document.getElementById("tutorial-div");
@@ -22,7 +186,7 @@ document.addEventListener("DOMContentLoaded", function () {
     "<p>Lo siento, dime rápido, me tengo que ir...</p>",
     "<p>... aquí estoy</p>",
     "<p> que irritante...!</p>",
-    "<p>...</p>",
+    "<p>...quiero estar solo/a</p>",
   ];
 
   function showElement(element) {
@@ -64,29 +228,33 @@ document.addEventListener("DOMContentLoaded", function () {
       showElement(answerDiv);
     }, 1000);
   }
-
-  updateTutorialDiv(0);
-  setTimeout(() => {
-    hideElement(tutorialDiv);
+function presentation(){
+    updateTutorialDiv(0);
     setTimeout(() => {
-      updateTalkDiv(0);
+      hideElement(tutorialDiv);
       setTimeout(() => {
-        hideElement(talkDiv);
+        updateTalkDiv(0);
         setTimeout(() => {
-          updateTalkDiv(1);
+          hideElement(talkDiv);
           setTimeout(() => {
-            hideElement(talkDiv);
+            updateTalkDiv(1);
             setTimeout(() => {
-              updateTutorialDiv(1);
+              hideElement(talkDiv);
               setTimeout(() => {
-                hideElement(tutorialDiv);
-              }, 7000);
-            }, 1000);
-          }, 3000);
-        }, 1000);
-      }, 3000);
-    }, 1000);
-  }, 12000);
+                updateTutorialDiv(1);
+                setTimeout(() => {
+                  hideElement(tutorialDiv);
+                }, 7000);
+              }, 1000);
+            }, 3000);
+          }, 1000);
+        }, 3000);
+      }, 1000);
+    }, 12000);
+}
+
+presentation();
+  
 
   options.forEach((option) => {
     option.addEventListener("change", () => {
@@ -118,6 +286,8 @@ document.addEventListener("DOMContentLoaded", function () {
       parkImg.style.filter = "saturate(10%)";
       updateAnswerDiv(5);
     } else if (effect === "llamando") {
+        parkImg.style.filter = "url('./assets/esc1.jpg')";
+      updateAnswerDiv(6);
     } else if (effect === "regalo") {
     } else if (effect === "discutiendo") {
     } else {
@@ -138,4 +308,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // Ejecutar función al cargar la página
   cambiarImagenSegunPantalla();
-});
+});*/
